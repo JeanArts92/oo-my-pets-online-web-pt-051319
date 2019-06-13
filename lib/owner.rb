2ca9341798d92357508342e4ab7471dfafc1be0e
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-13 15:41:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :name
+  attr_reader :species
+  
+  @@all = []
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+  end 
+  
+  def self.all 
+    
+  end 
+  
+  def say_species
+    "I am a #{species}."
+  end 
+  
+  def buy_fish(fish)
+   
+  end 
+    
+  
+  
+  
 end
