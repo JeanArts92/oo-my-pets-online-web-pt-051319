c31a536d40106576351143c95f23658a0c603001
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-13 15:42:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,14 +2,14 @@ class Owner
   attr_accessor :pets, :name
   attr_reader :species
   
-  @@all = []
+  
   def initialize(species)
     @species = species
     @pets = {fishes: [], cats: [], dogs: []}
   end 
   
   def self.all 
-    
+    pets.self.each
   end 
   
   def say_species
