ba68e07604b7e005c88bea660cfe1a49c76f1466
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-13 16:44:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,6 @@
 class Owner
   attr_reader :species
-  attr_accessor :name, :pets, :fish, :dog, :cat
+  attr_accessor :name, :pets, :fish, :dog, :cat, :mood
   
   @@all = []
   @pets = {fishes: [], cats: [], dogs: []}
